5e8595a2a1189cda18f1c9b4c2044277aa08ad38
charivna <124678194+charivna@users.noreply.github.com>
2023-07-18 10:11:36 +0200

    push

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-// import axios from "axios";
-// axios.defaults.headers.common["x-api-key"] = "38322370-cd2680a408cbe7ab4bd12cfc4"
+
+import axios from "axios";
 
 const refs = {
     formEl : document.querySelector('.search-form'),
     keyWordEl : document.querySelector('input')
 }
 
-console.log(keyWordEl);
-const BASE_URL = "https://pixabay.com/api/"
+refs.formEl.addEventListener('submit', onSubmit)
+
+function onSubmit(evt) {
+
+    evt.preventDefault()
+    
+ const BASE_URL = "https://pixabay.com/api/"
 const API_KEY = "38322370-cd2680a408cbe7ab4bd12cfc4"
 
+    return fetch(`${BASE_URL}?key=${API_KEY}&q=${refs.keyWordEl.value}&image-type=photo&orientation=horizontal&safesearch=true`)
+        .then(resp => resp.json())
+       
+}
+
+onSubmit(evt).then(data => console.log(data))
