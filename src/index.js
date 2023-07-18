c667eff366ca55338dc431d23a4e8eeab76e9fa7
charivna <124678194+charivna@users.noreply.github.com>
2023-07-18 09:20:17 +0200

    test

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// import axios from "axios";
+// axios.defaults.headers.common["x-api-key"] = "38322370-cd2680a408cbe7ab4bd12cfc4"
+
+const refs = {
+    formEl : document.querySelector('.search-form'),
+    keyWordEl : document.querySelector('input')
+}
+
+console.log(keyWordEl);
+const BASE_URL = "https://pixabay.com/api/"
+const API_KEY = "38322370-cd2680a408cbe7ab4bd12cfc4"
+
