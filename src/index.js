0c9f39b0c84e6e23d8c52f5bd8857604a562c81f
charivna <124678194+charivna@users.noreply.github.com>
2023-07-19 15:35:13 +0200

    add axios

--- src/index.js
+++ src/index.js
@@ -11,13 +11,33 @@ refs.formEl.addEventListener('submit', onSubmit)
 function onSubmit(evt) {
 
     evt.preventDefault()
-    
- const BASE_URL = "https://pixabay.com/api/"
-const API_KEY = "38322370-cd2680a408cbe7ab4bd12cfc4"
+    fetchImages()
 
-    return fetch(`${BASE_URL}?key=${API_KEY}&q=${refs.keyWordEl.value}&image-type=photo&orientation=horizontal&safesearch=true`)
-        .then(resp => resp.json())
-       
 }
 
-onSubmit(evt).then(data => console.log(data))
+async function fetchImages() {
+const BASE_URL = "https://pixabay.com/api/"
+    const API_KEY = "38322370-cd2680a408cbe7ab4bd12cfc4"
+    
+    const response = await axios.get(`${BASE_URL}`,
+        {
+            params: {
+                key:`${API_KEY}`,
+                q:`${refs.keyWordEl.value}`,
+image_type: "photo",
+orientation:"horizontal",
+safesearch:"true" 
+            }
+        }).then(resp => {
+            if (resp.length <= 0) {
+            throw new Error ("Sorry, there are no images matching your search query. Please try again.")
+            }
+            console.log(resp.data);
+
+        }).catch(err => console.log(err))
+}
+
+function createMarkup(response) {
+    
+}
+
